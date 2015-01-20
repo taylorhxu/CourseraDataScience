5a113c70388c5ab579eb2cdf9ddb187f52a172be
Taylor Xu <taylor.h.xu@gmail.com>
2015-01-20 20:41:57 +0000

    update

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,14 +1,14 @@
-pollutantmean <- function (directory, pollutant, id = 1:332){
-	files <-list.files(directory, full.name=TRUE)
-	data <- data.frame()
-	for (i in id){
-		data <- rbind(data, read.csv(files[i]))
-	}
-	if (pollutant == "nitrate"){
-		mean(data$nitrate, na.rm=TRUE)
-		
-		else{
-			mean(data$sulphate, na.rm=TRUE)
-		}
-	}
+pollutantmean <- function(directory, pollutant, id = 1:332){
+        
+        files <-list.files(directory, full.name=TRUE)
+        data <-  data.frame()
+        for (i in id) {
+                data <- rbind (data, read.csv(files[i]))
+        }
+        if (pollutant == "sulfate") {
+        mean(data$sulfate, na.rm=TRUE)
+        }
+        else {
+              mean(data$nitrate, na.rm=TRUE)
+        }
 }
